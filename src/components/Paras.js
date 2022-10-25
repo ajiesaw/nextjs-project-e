c36aaf3899ccb3ccc68218b4ad873e2242a2a766
ajiesaw <ajiesaw1612@gmail.com>
2022-10-25 17:59:15 +0700

    Menambahkan navbar pada file Paras.js

--- src/components/Paras.js
+++ src/components/Paras.js
@@ -1,9 +1,19 @@
 const Paras = () => {
   return (
-    <div className="text-black">
+    /*<div className="text-black">
       <p>Paras Frontend Intern Hiring Test</p>
       <p>Please modify the /src based on the test</p>
-    </div>
+    </div>*/
+    <nav className="flex sm:justify-center space-x-4">
+  {[
+    ['Home', '/dashboard'],
+    ['Team', '/team'],
+    ['Projects', '/projects'],
+    ['Reports', '/reports'],
+  ].map(([title, url]) => (
+    <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+  ))}
+</nav>
   )
 }
 
