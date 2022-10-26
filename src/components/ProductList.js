f7fbea210491675de5990d2c5c59dc2a23711418
ajiesaw <ajiesaw1612@gmail.com>
2022-10-26 17:01:49 +0700

    Memindahkan const pada file ProductList.js ke file baru tabsData.json

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -24,29 +24,22 @@ export default ProductList;*/
 
 import { useState } from "react";
 import Paras from "./Paras";
-
-const tabsData = [
-  {
-    label: "This Text",
-    content:
-      "Ut irure mollit nulla eiusmod excepteur laboris elit sit anim magna tempor excepteur labore nulla.",
-  },
-  {
-    label: "That Text",
-    content:
-      "Fugiat dolor et quis in incididunt aute. Ullamco voluptate consectetur dolor officia sunt est dolor sint.",
-  },
-];
+import tabsData from "../dummy/tabsData"
 
 export function ProductList() {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   return (
     <>
+    {/*Header dan Navbar*/}
     <Paras />
+
+    {/*Gambar*/}
     <div>
       <img class="object-cover w-full h-96" src="https://akcdn.detik.net.id/community/media/visual/2021/12/25/shopee-1_169.png?w=700&q=90" />
     </div>
+
+    {/*Tab*/}
     <div>
       <div className="flex space-x-4 py-8 justify-center">
         {/* Loop through tab data and render button for each. */}
