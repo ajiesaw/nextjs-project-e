a49b83b5e90b33fbd6ac6c73be5ebb5f4478f263
ajiesaw <ajiesaw1612@gmail.com>
2022-10-26 19:31:49 +0700

    Menghapus tab pada file ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -24,7 +24,6 @@ export default ProductList;*/
 
 import { useState } from "react";
 import Paras from "./Paras";
-import tabsData from "../dummy/tabsData"
 
 export function ProductList() {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
@@ -37,32 +36,6 @@ export function ProductList() {
     {/*Gambar*/}
     <div>
       <img class="object-cover w-full h-96" src="https://akcdn.detik.net.id/community/media/visual/2021/12/25/shopee-1_169.png?w=700&q=90" />
-    </div>
-
-    {/*Tab*/}
-    <div>
-      <div className="flex space-x-4 py-8 justify-center">
-        {/* Loop through tab data and render button for each. */}
-        {tabsData.map((tab, idx) => {
-          return (
-            <button
-              key={idx}
-              className={`text-gray-500 hover:text-gray-900 ${
-                idx === activeTabIndex
-                  ? "text-gray-900"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-              // Change the active tab on click.
-              onClick={() => setActiveTabIndex(idx)}>
-              {tab.label}
-            </button>
-          );
-        })}
-      </div>
-      {/* Show active tab content. */}
-      <div className="flex justify-center py-0">
-        <p>{tabsData[activeTabIndex].content}</p>
-      </div>
     </div>
     </>
   );
