80f0eca867cfb7bd705e4530782a8082054af76f
ajiesaw <ajiesaw1612@gmail.com>
2022-10-26 15:26:27 +0700

    Penambahan gambar pada file ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,6 +5,9 @@ const ProductList = () => {
   return (
     <>
       <Paras />
+      <div>
+      <img class="object-cover w-full h-96" src="https://akcdn.detik.net.id/community/media/visual/2021/12/25/shopee-1_169.png?w=700&q=90" />
+    </div>
       <div className="mt-4">
         <p>This is the List of Product</p>
         <Link key={1} to={`/products/1`} className="link">
