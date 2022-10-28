50c1a3d224b855ff7609e33a535d8a8db0ee7b9b
ajiesaw <ajiesaw1612@gmail.com>
2022-10-28 12:23:54 +0700

    Menambahkan state jika salah satu dropdown true, maka yang lain false pada file Parass.js

--- src/components/Paras.js
+++ src/components/Paras.js
@@ -12,30 +12,30 @@ const Paras = () => {
       <div className="flex flex-row justify-between border-b-2 pb-2">
         <div className="flex flex-row divide-x">
           <div>
-          <button onClick={()=>setLang(!lang)} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
+          <button onClick={()=>{setLang(!lang); setCur(false); setMenu(false)}} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
               Language
             </button>
             {lang &&
-              <div className="absolute bg-white top-10 shadow-xl rounded-lg">
-                <ul className='py-1 px-1 flex flex-col'>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>English</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Indonesia</li>
+              <div className="absolute bg-white left-2 top-10 shadow-xl rounded-lg">
+                <ul className='py-3 px-3 flex flex-col'>
+                  <li onClick={()=>setLang(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>English</li>
+                  <li onClick={()=>setLang(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Indonesia</li>
                 </ul>
               </div>
-              }
+            }
           </div>
           <div>
-          <button onClick={()=>setCur(!cur)} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
+          <button onClick={()=>{setCur(!cur); setLang(false); setMenu(false)}} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
               Currency
             </button>
             {cur &&
-              <div className="absolute bg-white left-32 top-10 shadow-xl rounded-lg">
-                <ul className='py-1 px-1 flex flex-col'>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>USD</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>IDR</li>
+              <div className="absolute bg-white left-28 top-10 shadow-xl rounded-lg">
+                <ul className='py-3 px-3 flex flex-col'>
+                  <li onClick={()=>setCur(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>USD</li>
+                  <li onClick={()=>setCur(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>IDR</li>
                 </ul>
               </div>
-              }
+            }
           </div>
           <div className="px-2">
             Call Us (123)4567890
@@ -62,22 +62,22 @@ const Paras = () => {
         </div>
         <div className="flex justify-end space-x-4 lg:space-x-8 px-2">
         <div className="md:hidden relative">
-            <button onClick={()=>setMenu(!menu)} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
+            <button onClick={()=>{setMenu(!menu); setLang(false); setCur(false)}} class="px-2 text-gray-900 peer focus:text-gray-500 hover:text-gray-500 transition-all duration-200">
               Menu
             </button>
             {menu &&
-              <div className="absolute bg-white -right-4 top-5 shadow-xl rounded-lg">
-                <ul className='py-1 px-1 flex flex-col'>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Home</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Shop</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Collection</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Pages</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Blog</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>About</li>
-                  <li className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Contact</li>
+              <div className="absolute bg-white -right-6 top-5 shadow-xl rounded-lg">
+                <ul className='py-3 px-3 flex flex-col'>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Home</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Shop</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Collection</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Pages</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Blog</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>About</li>
+                  <li onClick={()=>setMenu(false)} className='cursor-pointer p-1 rounded-md text-gray-900 hover:text-gray-500 text-center'>Contact</li>
                 </ul>
               </div>
-              }
+            }
           </div>
           <div className="cursor-pointer text-gray-900 hover:text-gray-500">Search</div>
           <div className="cursor-pointer text-gray-900 hover:text-gray-500">Cart</div>
