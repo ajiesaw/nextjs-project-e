d1f934d59c69532578e0417cf976f4f127901760
ajiesaw <ajiesaw1612@gmail.com>
2022-10-28 12:54:04 +0700

    Memperbaiki tampilan responsive pada file ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -284,13 +284,13 @@ export function ProductList() {
 
     {/*Product List*/}
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl py-16 lg:max-w-7xl px-4">
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group cursor-pointer">
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8">
+            <a key={product.id} href={product.href} className="group">
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-white xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.image}
                   alt={product.imageAlt}
@@ -298,7 +298,7 @@ export function ProductList() {
                 />
               </div>
               <h3 className="truncate mt-4 text-sm text-gray-700">{product.title}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$ {product.price}</p>
+              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
             </a>
           ))}
         </div>
