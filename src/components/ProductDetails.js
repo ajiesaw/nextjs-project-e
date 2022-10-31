8551d7d73b48f46565bd6e38d08ecdd0d1cd605f
ajiesaw <ajiesaw1612@gmail.com>
2022-10-31 18:01:57 +0700

    Menambahkan breadcrumbs pada file ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,9 +1,29 @@
 import { Link, useParams } from "react-router-dom";
+import Paras from "./Paras";
+import products from "../dummy/products.json"
 
 const ProductDetails = () => {
   const { productId } = useParams();
 
   return (
+    <>
+    <Paras />
+    
+    {/* Breadcrumbs */}
+    <div className="flex flex-row px-2 py-4 space-x-2 items-center">
+      <div className="cursor-pointer text-gray-900 hover:text-gray-500">
+        <Link to="/">
+          Home
+        </Link>
+      </div>
+      <div className="cursor-default">
+        /
+      </div>
+      <div>
+        Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+      </div>
+    </div>
+
     <div>
       <p>
         This is the <b>{productId}</b> details
@@ -12,6 +32,7 @@ const ProductDetails = () => {
         Back to List
       </Link>
     </div>
+    </>
   );
 };
 
