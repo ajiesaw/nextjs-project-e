8b081fbca4f02161f92903748594739c6f70b968
ajiesaw <ajiesaw1612@gmail.com>
2022-10-31 13:45:55 +0700

    Mengubah tampilan product list menjadi minimal 2 kolom pada container kecil pada file ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -65,7 +65,7 @@ const ProductList = () => {
           <div className="mx-auto max-w-2xl py-16 lg:max-w-7xl px-4">
             <h2 className="sr-only">Products</h2>
 
-            <div className="grid grid-cols-1 gap-y-10 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <div className="grid gap-y-10 gap-x-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {products.map((product) => (
                 tabs === "All Products" ? (
                 <a key={product.id} href={product.href} className="group">
